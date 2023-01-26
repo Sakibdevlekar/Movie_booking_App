aec5408576a275da63d0feab90961c6b2a3ac166
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-01-26 21:11:11 +0530

    added middelwear for movie validation and added update delet apis for movie

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -22,3 +22,35 @@ exports.getMovie = async (req,res)=>{
     });
     res.status(200).send(movie)
 }
+
+exports.updateMovie = async (req, res)=>{
+    const id= req.params.id;
+
+    const savedMovie = await Movie.findOne({_id:id});
+    
+    if(!savedMovie){
+        res.status(400).send("Movie to be updated doesn't exists");
+    }
+//turnery operator 
+    savedMovie.name = req.body.name ? req.body.name : savedMovie.name;
+    savedMovie.description = req.body.description ? req.body.description : savedMovie.description;
+    savedMovie.casts = req.body.casts ? req.body.casts : savedMovie.casts;
+    savedMovie.director = req.body.director ? req.body.director : savedMovie.director;
+    savedMovie.trailerUrl = req.body.trailerUrl ? req.body.trailerUrl : savedMovie.trailerUrl;
+    savedMovie.posterUrl = req.body.posterUrl ? req.body.posterUrl : savedMovie.posterUrl;
+    savedMovie.language = req.body.language ? req.body.language : savedMovie.language;
+    savedMovie.releaseDate = req.body.releaseDate ? req.body.releaseDate : savedMovie.releaseDate;
+    savedMovie.releaseStatus = req.body.releaseStatus ? req.body.releaseStatus : savedMovie.releaseStatus;
+
+    const updatedMovie = await savedMovie.save();
+
+    res.status(200).send(updatedMovie);
+
+}
+
+exports.deleteMovie = async (req, res)=>{
+    await Movie.findByIdAndDelete({
+        _id:req.params.id
+    });
+    res.status(200).send({message:`Successfully delete movie with id:${req.params.id}`})
+}
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const routes = express.Router()
 const movieController = require ('../controllers/movie.controller')
+const MovieRequestValidator= require('../middlewear/validateMovieRequest')
 
-routes.post('/MovieBookingApp/api/v1/create/Movies',movieController.createMovie)
+routes.post('/MovieBookingApp/api/v1/create/Movies',[MovieRequestValidator.validateMovieRequest],movieController.createMovie)
 routes.get('/MovieBookingApp/api/v1/Movies',movieController.getAllMovie)
 routes.get('/MovieBookingApp/api/v1/Movies/:id',movieController.getMovie)
-
-
+routes.put('/MovieBookingApp/api/v1/Movies/:id',movieController.updateMovie)
+routes.delete('/MovieBookingApp/api/v1/Movies/:id',movieController.deleteMovie)
 
 
 

f82a010be3c8438fec38c3e8f8628fcd86c95ebe
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-02-07 22:28:50 +0530

    theater api

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -30,3 +30,57 @@ exports.getAllTheatre =async(req,res)=>{
     const theaters =  await Theatre.find(queryObj);
     res.status(201).send(theaters);
 }
+
+exports.getTheatre =async(req,res)=>{
+    const id = req.params.id;
+
+    const theatre = await Theatre.findOne({
+        _id:id
+    })
+    res.status(200).send(theatre)
+}
+
+exports.deleteTheatre =async(req,res)=>{
+
+await Theatre.deleteOne({
+        _id : req.params.id
+    });
+    res.status(200).send({message:`Successfully deleted  theatre with id: ${req.params.id}`});
+}
+
+exports.updateTheatre = async(req, res)=>{
+    const id = req.params.id;
+
+    const saveTheater = await Theatre.findOne({_id:req.params.id});
+    if(!saveTheater){
+        return res.status(400).send({message:"Theater does't exists"})
+    }
+    saveTheater.name = req.body.name ? req.body.name : saveTheater.name;
+    saveTheater.description = req.body.description ? req.body.description : saveTheater.description;
+    saveTheater.city = req.body.city ? req.body.city : saveTheater.city;
+    saveTheater.pinCode = req.body.pinCode ? req.body.pinCode : saveTheater.pinCode;
+
+    const updateTheatre = await saveTheater.save();
+    res.status(200).send(updateTheatre);
+}
+
+exports.AddMoviesToTheater = async (req, res) =>{
+    const theaterId = req.params.theaterId;
+    const movieId = req.params.movieId
+
+    const saveTheater = await Theatre.findOne({_id:theaterId})
+
+    if (!saveTheater){
+        return res.status(400).send({message:"Theater doesn't exists"})
+    }
+
+    const movieIds =  req.body.movies;
+
+    movieIds.forEach(movieId => {
+        saveTheater.movies.push(movieId)
+        
+    });
+    const updateTheatre = await saveTheater.save();
+
+    return res.status(200).send(updateTheatre);
+}
--- routes/theatre.rout.js
+++ routes/theatre.rout.js
@@ -6,6 +6,9 @@ const theatreController = require('../controllers/theatre.controller')
 
 routes.post('/MovieBookingApp/api/v1/theaters',theatreController.createTheatre );
 routes.get('/MovieBookingApp/api/v1/theaters',theatreController.getAllTheatre );
+routes.get('/MovieBookingApp/api/v1/theater/:id',theatreController.getTheatre );
+routes.delete('/MovieBookingApp/api/v1/theater/:id',theatreController.deleteTheatre );
+routes.put('/MovieBookingApp/api/v1/theater/:id',theatreController.updateTheatre);
+routes.put('/MovieBookingApp/api/v1/theater/:theaterId/movies',theatreController.AddMoviesToTheater);
 
-
-module.exports = {TheatreRoutes : routes}
+module.exports = {TheatreRoutes : routes} 
